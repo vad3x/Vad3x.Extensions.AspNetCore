c8003afa3e920cf38de47ca4e7e10892eca5cc1b
Vadim Orekhov <vorechov@svyaznoy.ru>
2018-05-21 14:31:43 +0100

    to ProblemDetails

--- src/Vad3x.Extensions.AspNetCore.Mvc/RestController.cs
+++ src/Vad3x.Extensions.AspNetCore.Mvc/RestController.cs
@@ -5,36 +5,36 @@ namespace Microsoft.AspNetCore.Mvc
     public abstract class RestController : Controller
     {
         [NonAction]
-        public IActionResult Conflict(string type, string title = null)
+        public ActionResult Conflict(string type, string title = null)
         {
             return GeneralResponse(StatusCodes.Status409Conflict, type, title);
         }
 
         [NonAction]
-        public IActionResult Gone(string type, string title = null)
+        public ActionResult Gone(string type, string title = null)
         {
             return GeneralResponse(StatusCodes.Status410Gone, type, title);
         }
 
         [NonAction]
-        public IActionResult NotFound(string type, string title = null)
+        public ActionResult NotFound(string type, string title = null)
         {
             return GeneralResponse(StatusCodes.Status404NotFound, type, title);
         }
 
         [NonAction]
-        public IActionResult Unauthorized(string type, string title = null)
+        public ActionResult Unauthorized(string type, string title = null)
         {
             return GeneralResponse(StatusCodes.Status401Unauthorized, type, title);
         }
 
         [NonAction]
-        public IActionResult Forbid(string type, string title = null)
+        public ActionResult Forbid(string type, string title = null)
         {
             return GeneralResponse(StatusCodes.Status403Forbidden, type, title);
         }
 
-        private IActionResult GeneralResponse(int statusCode, string type, string title)
+        private ActionResult GeneralResponse(int statusCode, string type, string title)
         {
             var problemDetails = new ProblemDetails
             {
