d60a03e0a74afe80874e33cce57d2d9c8698865a
Vadim Orekhov <vorechov@svyaznoy.ru>
2018-08-08 17:47:06 +0100

    added ObjectResults

--- src/Vad3x.Extensions.AspNetCore.Mvc/RestController.cs
+++ src/Vad3x.Extensions.AspNetCore.Mvc/RestController.cs
@@ -5,31 +5,31 @@ namespace Microsoft.AspNetCore.Mvc
     public abstract class RestController : Controller
     {
         [NonAction]
-        public ActionResult Conflict(string type, string title = null)
+        public ConflictRestObjectResult Conflict(string type, string title = null)
         {
             return new ConflictRestObjectResult(type, title, HttpContext.Request.Path);
         }
 
         [NonAction]
-        public ActionResult Gone(string type, string title = null)
+        public GoneRestObjectResult Gone(string type, string title = null)
         {
             return new GoneRestObjectResult(type, title, HttpContext.Request.Path);
         }
 
         [NonAction]
-        public ActionResult NotFound(string type, string title = null)
+        public NotFoundRestObjectResult NotFound(string type, string title = null)
         {
             return new NotFoundRestObjectResult(type, title, HttpContext.Request.Path);
         }
 
         [NonAction]
-        public ActionResult Unauthorized(string type, string title = null)
+        public UnauthorizedRestObjectResult Unauthorized(string type, string title = null)
         {
             return new UnauthorizedRestObjectResult(type, title, HttpContext.Request.Path);
         }
 
         [NonAction]
-        public ActionResult Forbid(string type, string title = null)
+        public ForbiddenRestObjectResult Forbid(string type, string title = null)
         {
             return new ForbiddenRestObjectResult(type, title, HttpContext.Request.Path);
         }
