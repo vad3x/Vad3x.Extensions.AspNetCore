2ae1acc5bdb8808889ca2b32e93d47b2ae552b46
Vadim Orekhov <vad3xs@gmail.com>
2018-10-19 16:32:32 +0100

    EF extensions

---
+++ src/Vad3x.Extensions.EntityFrameworkCore/MigrationHelpers.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyModel;
+
+namespace Vad3x.Extensions.EntityFrameworkCore
+{
+    public static class MigrationHelpers
+    {
+        public static Type GetDbContextType(string dbContextName)
+        {
+            var types = GetAllTypes(dbContextName);
+            if (types.Count() != 1)
+            {
+                throw new ArgumentException($"problem with finding `${dbContextName}`");
+            }
+
+            return types.First();
+        }
+
+        public static IEnumerable<Type> GetAllTypes(string name)
+        {
+            var platform = Environment.OSVersion.Platform.ToString();
+            var runtimeAssemblyNames = DependencyContext.Default.GetRuntimeAssemblyNames(platform);
+
+            return runtimeAssemblyNames
+                .Select(Assembly.Load)
+                .SelectMany(a => a.ExportedTypes)
+                .Where(t => t.Name == name);
+        }
+
+        public static string[] PendingMigrations(IWebHost webHost, Type dbContextType)
+        {
+            using (var scope = webHost.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                var dbContext = services.GetService(dbContextType) as DbContext;
+                var migrationsAssembly = dbContext.GetService<IMigrationsAssembly>();
+                var historyRepository = dbContext.GetService<IHistoryRepository>();
+
+                var all = migrationsAssembly.Migrations.Keys;
+
+                IEnumerable<string> pending;
+                if (historyRepository.Exists())
+                {
+                    var applied = historyRepository.GetAppliedMigrations().Select(r => r.MigrationId);
+                    pending = all.Except(applied);
+                }
+                else
+                {
+                    pending = all.Prepend("0");
+                }
+
+                return pending.ToArray();
+            }
+        }
+
+        public static void Migrate(IWebHost webHost, Type dbContextType)
+        {
+            using (var scope = webHost.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                var dbContext = services.GetService(dbContextType) as DbContext;
+
+                dbContext.Database.Migrate();
+            }
+        }
+    }
+}
---
+++ src/Vad3x.Extensions.EntityFrameworkCore/MigratorWebHostExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+using Microsoft.AspNetCore.Hosting;
+
+namespace Vad3x.Extensions.EntityFrameworkCore
+{
+    public static class MigratorWebHostExtensions
+    {
+        public static string Migrator(this IWebHost webHost, string[] args)
+        {
+            if (args.Contains("--pending-migrations"))
+            {
+                var dbContextName = args.First(x => !x.StartsWith("--"));
+
+                var pending = MigrationHelpers.PendingMigrations(webHost, MigrationHelpers.GetDbContextType(dbContextName));
+
+                if (pending.Any())
+                {
+                    if (args.Contains("--first-last"))
+                    {
+                        var first = pending.First();
+                        var last = pending.Last();
+
+                        pending = new[] { first, last };
+                    }
+
+                    if (args.Contains("--first"))
+                    {
+                        pending = new[] { pending.First() };
+                    }
+                }
+
+                return string.Join(" ", pending);
+            }
+            else if (args.Contains("--migrate"))
+            {
+                var dbContextName = args.First(x => !x.StartsWith("--"));
+
+                MigrationHelpers.Migrate(webHost, MigrationHelpers.GetDbContextType(dbContextName));
+
+                return string.Empty;
+            }
+
+            return null;
+        }
+    }
+}
