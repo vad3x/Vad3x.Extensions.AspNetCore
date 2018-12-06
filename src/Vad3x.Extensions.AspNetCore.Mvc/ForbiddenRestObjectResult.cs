7a5adada02ecd0c34ed5518fcdc49e23017bb924
Vadim Orekhov <vad3xs@gmail.com>
2018-12-06 10:50:05 +0000

    added DefaultStatusCode

--- src/Vad3x.Extensions.AspNetCore.Mvc/ForbiddenRestObjectResult.cs
+++ src/Vad3x.Extensions.AspNetCore.Mvc/ForbiddenRestObjectResult.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 namespace Microsoft.AspNetCore.Mvc
 {
+    [DefaultStatusCode(DefaultStatusCode)]
     public class ForbiddenRestObjectResult : RestObjectResult
     {
+        private const int DefaultStatusCode = StatusCodes.Status403Forbidden;
+
         public ForbiddenRestObjectResult(string type, string title, string instance)
-            : base(StatusCodes.Status403Forbidden, type, title, instance)
+            : base(DefaultStatusCode, type, title, instance)
         {
         }
     }
--- src/Vad3x.Extensions.AspNetCore.Mvc/GoneRestObjectResult.cs
+++ src/Vad3x.Extensions.AspNetCore.Mvc/GoneRestObjectResult.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 namespace Microsoft.AspNetCore.Mvc
 {
+    [DefaultStatusCode(DefaultStatusCode)]
     public class GoneRestObjectResult : RestObjectResult
     {
+        private const int DefaultStatusCode = StatusCodes.Status410Gone;
+
         public GoneRestObjectResult(string type, string title, string instance)
-            : base(StatusCodes.Status410Gone, type, title, instance)
+            : base(DefaultStatusCode, type, title, instance)
         {
         }
     }
